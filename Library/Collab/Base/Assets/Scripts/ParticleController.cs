7340e440542cc407e3c6b239dd9194f33a5df5c4
YUCHUAN LIN <Eggric0802@gmail.com>
2020-02-01 03:11:34 -0600

    First Commit

---
+++ Library/Collab/Base/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem particleLauncger;
+    public bool isTriggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isTriggered);
+        if(isTriggered)
+        {
+            particleLauncger.Emit(1);
+        }
+    }
+}
---
+++ Library/Collab/Original/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public ParticleSystem particleLauncger;
+    public bool isTriggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isTriggered);
+        if(isTriggered)
+        {
+            particleLauncger.Play();
+        }
+        else
+        {
+            particleLauncger.Stop();
+        }
+    }
+}
